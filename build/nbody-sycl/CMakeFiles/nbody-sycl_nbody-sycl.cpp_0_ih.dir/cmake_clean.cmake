c132bb3d634d0f8499e2eecbb15c44c41e6d5b13
Georgi <georgimweb@gmail.com>
2020-11-24 10:05:39 +0000

    Remove build/ from tracked

--- build/nbody-sycl/CMakeFiles/nbody-sycl_nbody-sycl.cpp_0_ih.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/nbody-sycl_nbody-sycl.cpp_0_ih"
-  "nbody-sycl.cpp.sycl"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/nbody-sycl_nbody-sycl.cpp_0_ih.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
